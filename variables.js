871c9d8e2b280c66470e14ae52fcf1084e95e84a
miloskuzmic <miloskuzmic@gmail.com>
2021-03-09 22:06:12 +0100

    Finised excercise

--- variables.js
+++ variables.js
@@ -120,5 +120,5 @@ console.log (age > 120);
 console.log (negative);
 /*15. Let’s say there are speed limitations on a motorway from 60 to 120 kilometers per hour. If we store the current speed value in the variable speed. Write an expression which will check if we are driving drive safely or not? (true if we are driving safe and false if not)   */
 var speed = 80;
-//var check = speed >= 60 && speed <= 120;
+
 console.log (speed >= 60 && speed <= 120);
